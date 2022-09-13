9b3a006052962299f223c124f070f7433497b578
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-09-14 01:54:17 +1000

    commit tree 416bc4a7647717b1ec785f4668fdca9ac60992bc

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+    array = (2...num).to_a
+    result = true
+    if  array.map{|x| num%x}.include?(0)
+        result = false
+    elsif num <= 1
+        result = false
+    else
+        result = true
+    end
+    result
+end
+
+
